649cf5cba6708d72dd01aa412ce6994a2c1099f1
Alexander Zhivkov <sashko.zhivkov@gmail.com>
2022-08-03 11:45:23 +0300

    Project initialization

---
+++ src/main/java/com/tinqin/academy/formulaonetinqincrud/FormulaOneTinqinCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.formulaonetinqincrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaOneTinqinCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormulaOneTinqinCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinqin/academy/formulaonetinqincrud/FormulaOneTinqinCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.formulaonetinqincrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaOneTinqinCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
